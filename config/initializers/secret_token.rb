f217184107d9a382e6e348f08d910e13a6e220b0
Taylor Smith <tsmith@neuone.com>
2013-06-17 09:03:31 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MappingWithGoogle::Application.config.secret_token = 'f1614b65c7ab7dc14f5f65a8216d601b739d81c0ca25e819dd04769c5b32c47296e2bbc140f29f3b090ddd04993c583e52a6ef73eb716e142cbf50e6d29da9f9'
